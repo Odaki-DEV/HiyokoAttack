0436362fe6eca0e387129f515ceb61c47819718f
Odaki-DEV <kadai.kaikou@gmail.com>
2021-10-07 17:10:02 +0900

    commit 2021_10_07

---
+++ Assets/ButtoonTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtoonTest : MonoBehaviour
+{
+
+    public void TestCall()
+    {
+        Debug.Log("Hello Unity");
+    }
+}
